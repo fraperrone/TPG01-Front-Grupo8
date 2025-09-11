438ec90dd6fd4db61b01e7e4cd06f1759c22d93f
Carlos Sebastian Gauto <23csebastian@gmail.com>
2025-09-11 15:22:11 -0300

    Agregado boton de /carlos.html

--- js/main.js
+++ js/main.js
@@ -7,3 +7,23 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const section = document.createElement("section");
+    section.id = "saludar";
+
+    const boton = document.createElement("button");
+    boton.textContent = "Saludar";
+    boton.classList.add("btn-saludo");
+
+    section.appendChild(boton);
+
+    const main = document.querySelector("main");
+    main.appendChild(section);
+
+    boton.addEventListener("click", () => {
+    alert("Hola, bienvenido a mi perfil!");
+    });
+});
+
+
